c9267d0890b7f34b251999466698e454a8556cc2
cat012 <50883405+cat012@users.noreply.github.com>
2023-09-14 03:49:16 +0300

    Add files via upload

--- esp32-wifi-radio-receiver.ino
+++ esp32-wifi-radio-receiver.ino
@@ -1,11 +1,10 @@
 // fox ^^ WiFi Radio Receiver alpha
-// 13 Sep 2023
+// 14 Sep 2023
 
 
 #include <Arduino.h>
 #include <Wire.h>
 #include <WiFi.h>
-#include <WiFiMulti.h>
 #include <WiFiAP.h>
 #include <WebServer.h>
 #include <EEPROM.h>
@@ -14,21 +13,6 @@
 #include <Adafruit_SSD1306.h>
 
 
-Audio audio(true, I2S_DAC_CHANNEL_BOTH_EN);
-WebServer server(80);
-WiFiMulti wifiMulti;
-
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 32
-#define OLED_RESET     -1
-#define SCREEN_ADDRESS 0x3C
-
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
-String current_station = "";
-uint8_t curstation=0;
-uint8_t audiovol=0;
-
 /* user settigs */
 const char *esp_softap_ssid = "esp32fox";
 const char *esp_softap_pass = "11223344Lap";
@@ -36,12 +20,12 @@ const char *esp_softap_pass = "11223344Lap";
 #define WIFI_CONN_TIME_SEC  8
 
 #define WIFI_SSID_PASS_SIZE   32
-#define MAX_STREAM_ADDR_SIZE  64
+#define STREAM_ADDRESS_SIZE  64
 
 #define STATIONS_LIST_SIZE  16
 /* end of user settigs */
 
-#define STREAMS_EE_SIZE  (STATIONS_LIST_SIZE * MAX_STREAM_ADDR_SIZE)
+#define STREAMS_EE_SIZE  (STATIONS_LIST_SIZE * STREAM_ADDRESS_SIZE)
 #define SETTIGS_EE_SIZE  2
 
 #define CURR_STATION_EE_ADDR  0
@@ -54,24 +38,43 @@ const char *esp_softap_pass = "11223344Lap";
 
 #define MAX_AUDIO_VOL  21
 
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 32
+#define OLED_RESET     -1
+#define SCREEN_ADDRESS 0x3C
 
-//-----------------------------------------------------------------------------
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+Audio audio(true, I2S_DAC_CHANNEL_BOTH_EN);
+WebServer server(80);
+
+String current_station = "";
+uint8_t curstation=0;
+uint8_t audiovol=0;
+bool audiomute=false;
+
+
+//-------------------------------------------------------------------------------------------------
 void handle_root(void)
     {
     String page="";
     page +="<html><head><meta charset=\"utf-8\"><title>WiFi Radio</title></head>";
     page +="<body bgcolor=\"GhostWhite\"><p><a href=\"/\">WiFi Radio Receiver alpha</a></p>";
     page +="<form action=\"/nextstation\"method=\"POST\">";
-    page +="<input type=\"submit\"name=\"prevsta\"value=\"Prev\">";
-    page +="<input type=\"submit\"name=\"nextsta\"value=\"Next\"></form>";
-    page +="Station: ";
-    page +=curstation; 
+    page +="<p><input type=\"submit\"name=\"amute\"value=\"Mute\">";
+    if(audiomute) page +=" <font color=\"FireBrick\">Muted</font>";
+    page +="</p>";
+    page +="<input type=\"submit\"name=\"firststa\"value=\"First\"> ";
+    page +="<input type=\"submit\"name=\"prevsta\"value=\"Prev\"> ";
+    page +=curstation+1;
+    page +=" <input type=\"submit\"name=\"nextsta\"value=\"Next\"> ";
+    page +="<input type=\"submit\"name=\"laststa\"value=\"Last\"></form>";
     page +="<form action=\"/changestation\" method=\"POST\">";
-    page +="<input type=\"text\"name=\"numstation\"size=\"10\"placeholder=\"0-";
-    page +=STATIONS_LIST_SIZE-1;
+    page +="<input type=\"text\"name=\"numstation\"size=\"5\"placeholder=\"1-";
+    page +=STATIONS_LIST_SIZE;
     page +="\"></br>";
     page +="<input type=\"submit\"value=\"Save\"></form>";
-    page +="Address: <font color=\"DimGray\">";
+    page +="<font color=\"DimGray\">";
     page +=current_station;
     page +="</font><form action=\"/replacestation\"method=\"POST\">";
     page +="<input type=\"text\"name=\"newstation\"size=\"40\"placeholder=\"New Address\"></br>";
@@ -80,7 +83,7 @@ void handle_root(void)
     page +="Volume: ";
     page +=audiovol;
     page +="<form action=\"/changevolume\"method=\"POST\">";
-    page +="<input type=\"text\" name=\"newvolume\"size=\"10\"placeholder=\"0-21\"></br>";
+    page +="<input type=\"text\" name=\"newvolume\"size=\"5\"placeholder=\"0-21\"></br>";
     page +="<input type=\"submit\"value=\"Save\"></form>";
     page +="SSID: ";
     page +=WiFi.SSID();
@@ -88,37 +91,47 @@ void handle_root(void)
     page +="<input type=\"text\"name=\"newssid\"placeholder=\"New SSID\"></br>"; 
     page +="<input type=\"text\"name=\"newpass\"placeholder=\"New Password\"></br>";
     page +="<input type=\"submit\"value=\"Save\"></form>";
+    page +="<form action=\"/systemrestart\"method=\"POST\">";
+    page +="<input type=\"submit\"name=\"srestart\"value=\"Restart\"></form>";
     page +="</body></html>";
 
     server.send(200, "text/html", page);
     }
 
 
-//-----------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------
 void handle_404(void)
     {
     server.send(404, "text/html", "<p>404: Not found</p><p><a href=\"/\">Home</a></p>");
     }
 
 
-//-----------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------
 void server_send_invalid_request(void)
     {
     server.send(400, "text/html", "<p>400: Invalid Request</p><p><a href=\"/\">Home</a></p>");
     }
 
 
-//--------------------------------------------------------------------------------------------
-void handle_nextstation(void)
+//-------------------------------------------------------------------------------------------------
+void handle_navstation(void)
     {
-    if(server.hasArg("nextsta")) { if(++curstation >= STATIONS_LIST_SIZE) curstation=0; }
+    if(server.hasArg("firststa")) { curstation=0; }
+    else if(server.hasArg("nextsta")) { if(++curstation >= STATIONS_LIST_SIZE) curstation=0; }
     else if(server.hasArg("prevsta")) { if(curstation>0) curstation--; else curstation=(STATIONS_LIST_SIZE-1); }
+    else if(server.hasArg("laststa")) { curstation=(STATIONS_LIST_SIZE-1); }
+    else if(server.hasArg("amute"))
+        {
+        if(audiomute) { audiomute=false; audiovol=EEPROM.readUChar(AUDIO_VOLUME_EE_ADDR); audio.setVolume(audiovol); }
+        else { audiomute=true; audio.setVolume(0); }
+        handle_root();
+        return;
+        }
     else { server_send_invalid_request(); return; }
 
-    EEPROM.writeUChar(CURR_STATION_EE_ADDR, curstation);
-    EEPROM.commit();
+    if(EEPROM.readUChar(CURR_STATION_EE_ADDR) != curstation)  { EEPROM.writeUChar(CURR_STATION_EE_ADDR, curstation); EEPROM.commit(); }
 
-    current_station = EEPROM.readString(STREAM_0_EE_ADDR+(curstation*MAX_STREAM_ADDR_SIZE));
+    current_station = EEPROM.readString(STREAM_0_EE_ADDR+(curstation*STREAM_ADDRESS_SIZE));
 
     audio.stopSong();
     audio.connecttohost((char*)current_station.c_str());
@@ -127,7 +140,7 @@ void handle_nextstation(void)
     }
 
 
-//--------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------
 void handle_changestation(void)
     {
     if(!server.hasArg("numstation") || server.arg("numstation")==NULL) { server_send_invalid_request(); return; }
@@ -137,12 +150,12 @@ void handle_changestation(void)
     str1.toCharArray(strbuff,4);
     uint8_t result=atoi(strbuff);
 
-    if(result < STATIONS_LIST_SIZE) { curstation = result; }
+    if(result <= STATIONS_LIST_SIZE && result >0) { curstation = result-1; }
     else { server_send_invalid_request(); return; }
 
     if(EEPROM.readUChar(CURR_STATION_EE_ADDR) != curstation)  { EEPROM.writeUChar(CURR_STATION_EE_ADDR, curstation); EEPROM.commit(); }
 
-    current_station = EEPROM.readString(STREAM_0_EE_ADDR+(curstation*MAX_STREAM_ADDR_SIZE));
+    current_station = EEPROM.readString(STREAM_0_EE_ADDR+(curstation*STREAM_ADDRESS_SIZE));
 
     audio.stopSong();
     audio.connecttohost((char*)current_station.c_str());
@@ -151,19 +164,19 @@ void handle_changestation(void)
     }
 
 
-//--------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------
 void handle_replacestation(void)
     {
     if(!server.hasArg("newstation") || server.arg("newstation")==NULL) { server_send_invalid_request(); return; }
 
     String str = server.arg("newstation");
-    char new_addr[MAX_STREAM_ADDR_SIZE] = "";
+    char new_addr[STREAM_ADDRESS_SIZE] = "";
     
     current_station = str;
 
-    str.toCharArray(new_addr, MAX_STREAM_ADDR_SIZE);
+    str.toCharArray(new_addr, STREAM_ADDRESS_SIZE);
 
-    EEPROM.writeString((STREAM_0_EE_ADDR+(curstation*MAX_STREAM_ADDR_SIZE)), new_addr);
+    EEPROM.writeString((STREAM_0_EE_ADDR+(curstation*STREAM_ADDRESS_SIZE)), new_addr);
     EEPROM.commit();
 
     audio.stopSong();
@@ -173,7 +186,7 @@ void handle_replacestation(void)
     }
 
 
-//--------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------
 void handle_show_stations(void)
     {
     String page="";
@@ -181,9 +194,9 @@ void handle_show_stations(void)
     page +="<p><a href=\"/\">Home</a></p>";
     for(uint8_t s=0; s<STATIONS_LIST_SIZE; s++)
         {
-        page += s;
+        page += s+1;
         page += ". <font color=\"DimGray\">";
-        page += EEPROM.readString(STREAM_0_EE_ADDR+(s*MAX_STREAM_ADDR_SIZE));
+        page += EEPROM.readString(STREAM_0_EE_ADDR+(s*STREAM_ADDRESS_SIZE));
         page += "</font></br>";
         }
     page +="<p><a href=\"/\">Home</a></p>";
@@ -192,7 +205,7 @@ void handle_show_stations(void)
     }
 
 
-//--------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------
 void handle_changevolume(void)
     {
     if(!server.hasArg("newvolume") || server.arg("newvolume")==NULL) { server_send_invalid_request(); return; }
@@ -206,14 +219,29 @@ void handle_changevolume(void)
     else { server_send_invalid_request(); return; }
 
     if(EEPROM.readUChar(AUDIO_VOLUME_EE_ADDR) != audiovol)  { EEPROM.writeUChar(AUDIO_VOLUME_EE_ADDR, audiovol); EEPROM.commit(); }
-
     audio.setVolume(audiovol);
+    audiomute=false;
 
     handle_root();
     }
 
 
-//--------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------
+void handle_system_restart(void)
+    {
+    if(!server.hasArg("srestart")) { server_send_invalid_request(); return; }
+    String page="";
+    page +="<body bgcolor=\"GhostWhite\">";
+    page +="Restart";
+    page +="<p><a href=\"/\">Home</a></p></body>";
+    server.send(200, "text/html", page);
+    audio.stopSong();
+    delay(1000);
+    ESP.restart();
+    }
+
+
+//-------------------------------------------------------------------------------------------------
 static void wifi_audio_init(char *ssid, char *pass)
     {
     WiFi.disconnect(true);
@@ -239,12 +267,12 @@ static void wifi_audio_init(char *ssid, char *pass)
 
         if(EEPROM.readUChar(AUDIO_VOLUME_EE_ADDR) > MAX_AUDIO_VOL)  { EEPROM.writeUChar(AUDIO_VOLUME_EE_ADDR, MAX_AUDIO_VOL); EEPROM.commit(); }
         audiovol=EEPROM.readUChar(AUDIO_VOLUME_EE_ADDR);
-        audio.setVolume(audiovol); // 0...21
+        audio.setVolume(audiovol);
 
         if(EEPROM.readUChar(CURR_STATION_EE_ADDR) >= STATIONS_LIST_SIZE)  { EEPROM.writeUChar(CURR_STATION_EE_ADDR, 0); EEPROM.commit(); }
         curstation=EEPROM.readUChar(CURR_STATION_EE_ADDR);
 
-        current_station = EEPROM.readString(STREAM_0_EE_ADDR+(curstation*MAX_STREAM_ADDR_SIZE));
+        current_station = EEPROM.readString(STREAM_0_EE_ADDR+(curstation*STREAM_ADDRESS_SIZE));
         audio.connecttohost((char*)current_station.c_str());
         }
     else
@@ -267,7 +295,7 @@ static void wifi_audio_init(char *ssid, char *pass)
     }
 
 
-//--------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------
 void handle_changeap(void)
     {
     if(!server.hasArg("newssid") || server.arg("newssid")==NULL || (!server.hasArg("newpass") || server.arg("newpass")==NULL)) { server_send_invalid_request(); return; }
@@ -295,7 +323,7 @@ void handle_changeap(void)
     }
 
 
-//############################################################################################################
+//#################################################################################################
 void setup(void)
     {
     Serial.begin(115200);
@@ -316,17 +344,18 @@ void setup(void)
 
     server.on("/", handle_root);
     server.on("/changestation", handle_changestation);
-    server.on("/nextstation", handle_nextstation);
+    server.on("/nextstation", handle_navstation);
     server.on("/replacestation", handle_replacestation);
     server.on("/changevolume", handle_changevolume);
     server.on("/changeap", handle_changeap);
     server.on("/stations", handle_show_stations);
+    server.on("/systemrestart", handle_system_restart);
     server.onNotFound(handle_404);
     server.begin();
     }
 
 
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 void loop(void)
     {
     audio.loop();
@@ -334,7 +363,7 @@ void loop(void)
     }
 
 
-//--------------------------------------------------------------------------------------------  
+//-------------------------------------------------------------------------------------------------
 void write_stream_title(String stitle)
     {
     display.clearDisplay();
@@ -345,7 +374,7 @@ void write_stream_title(String stitle)
     }
 
 
-//................................................................................
+//.................................................................................................
 void audio_info(const char *info)
     {
     Serial.print("audio info: ");
@@ -353,7 +382,7 @@ void audio_info(const char *info)
     }
 
 
-//................................................................................
+//..................................................................................................
 void audio_showstreamtitle(const char *info)
     {
     String sinfo=String(info);
